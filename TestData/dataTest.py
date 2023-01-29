0a6544a829d8d35cced1b05ded7127c038c44353
SectumSempra99 <kennyrmdhn@gmail.com>
2023-01-30 01:13:33 +0700

    Data Driven Test

--- TestData/dataTest.py
+++ TestData/dataTest.py
@@ -2,11 +2,11 @@ import sys
 sys.path.append('C:/Users/Qoin/OneDrive - PT. Loyalty Program Indonesia/Documents/MoonlayWeb')
 class dataForm():
 
-    FirstName = "Kenny"
-    LastName = "Ramadhan"
-    Email = "kennyrmdhn@gmail.com"
-    PhoneNumber = "082186460054"
-    Services = "Brain Resources"
-    Message = "Testing"
-    URL = "https://moonlay.com/contact-us/"
-    SuccessMessage = "Thank you for your message. We will get in touch with you shortly"
+    FirstName        = "Kenny"
+    LastName         = "Ramadhan"
+    Email            = "kennyrmdhn@gmail.com"
+    PhoneNumber      = "082186460054"
+    Services         = "Brain Resources"
+    Message          = "Testing"
+    URL              = "https://moonlay.com/contact-us/"
+    SuccessMessage   = "Thank you for your message. We will get in touch with you shortly"
